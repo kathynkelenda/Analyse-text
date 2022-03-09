9099e551a7f6af0ce017ca6f2fbce3d381489fcb
kathynkelenda <84560080+kathynkelenda@users.noreply.github.com>
2022-03-09 17:13:22 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-var textarea = document.getElementById("in").value;
+var textarea = document.getElementById("recup").value;
 
 const valid = () => {
 
@@ -8,7 +8,30 @@ const valid = () => {
     document.querySelector('.limite').appendChild(div_message);*/
    
     function traitement(){
-        var textarea = document.getElementById("in").value;
+        var textarea = document.getElementById("recup").value;
+        let div_message = document.createElement('div');
+        div_message.innerHTML = message;
+        document.querySelector('.mess').appendChild(div_message)
+
+        // Récuperer le message saisi dans textarea
+       
+        // Par une méthode existante, évaluer la longueur du texte saisi 
+
+        /*Parcourir toute la chaine de caractère pour recuperer les espaces
+           afin de déterminer le nombre de mots existant*/
+
+        /*Parcourir toute la chaine de caractère en comparant chaque 
+         caractère à la lettre "e" tout en incrémentant un compteur 
+         à chaque fois qu'on rencontre un "e". La valeur du compteur
+          est le nombre de fois que "e" existe dans la chaine de caractère.*/
+
+        /*Prendre chaque lettre de la chaine et parcourir et la comparer avec 
+        le reste du tableau tout en utilisant un compteur pour incrémenter
+        afin de savoir en définitive combien de fois une lettre est repeté dans 
+        la chaine.*/
+
+
+        
     }
 
 }

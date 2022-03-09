a6c4d1c046b5b5f5470e73244dc7e3a095345376
kathynkelenda <84560080+kathynkelenda@users.noreply.github.com>
2022-03-09 16:38:28 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+var textarea = document.getElementById("in").value;
+
+const valid = () => {
+
+    /*let div_message = document.createElement('div');
+    div_message.innerHTML = message;
+    document.querySelector('.limite').appendChild(div_message);*/
+   
+    function traitement(){
+        var textarea = document.getElementById("in").value;
+    }
+
+}
